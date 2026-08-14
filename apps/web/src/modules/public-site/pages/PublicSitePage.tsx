e2ecd862f42aae26cca99db56623703b1b95a277
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-14 02:27:48 -0300

    feat: compose public Visa Facil page

---
+++ apps/web/src/modules/public-site/pages/PublicSitePage.tsx
@@ -0,0 +1,34 @@
+import { useRef } from 'react';
+import { ContactSection } from '../components/ContactSection';
+import { DifferenceSection } from '../components/DifferenceSection';
+import { ExperienceSection } from '../components/ExperienceSection';
+import { FaqSection } from '../components/FaqSection';
+import { HeroSection } from '../components/HeroSection';
+import { PainPointsSection } from '../components/PainPointsSection';
+import { ProcessSection } from '../components/ProcessSection';
+import { PublicFooter } from '../components/PublicFooter';
+import { PublicHeader } from '../components/PublicHeader';
+import { ServicesSection } from '../components/ServicesSection';
+import { usePublicSiteInteractions } from '../usePublicSiteInteractions';
+
+export function PublicSitePage() {
+  const rootRef = useRef<HTMLDivElement>(null);
+  usePublicSiteInteractions(rootRef);
+
+  return (
+    <div ref={rootRef}>
+      <PublicHeader />
+      <main>
+        <HeroSection />
+        <ServicesSection />
+        <ExperienceSection />
+        <PainPointsSection />
+        <ProcessSection />
+        <DifferenceSection />
+        <FaqSection />
+        <ContactSection />
+      </main>
+      <PublicFooter />
+    </div>
+  );
+}
