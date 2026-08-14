53171186107238145c87dbef479558dee0f77508
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-14 09:50:19 -0300

    style: load processes module stylesheet

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -6,6 +6,7 @@ import './modules/public-site/styles/02-sections-responsive.css';
 import './modules/public-site/styles/03-hero-v3.css';
 import './modules/management/management.css';
 import './modules/clients/clients.css';
+import './modules/processes/processes.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
