bc91a4e0b00cf487e5156b78b188f534cedb7bbb
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-14 02:31:26 -0300

    fix: load migrated public site style cascade

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RootApplication } from './RootApplication';
-import './modules/public-site/styles/visa-facil.css';
+import './modules/public-site/styles/01-base.css';
+import './modules/public-site/styles/02-sections-responsive.css';
+import './modules/public-site/styles/03-hero-v3.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
