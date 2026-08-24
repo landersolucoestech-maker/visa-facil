44c8b4a52ae634da816f04cc74a5fc222bb01af6
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-24 18:00:00 -0300

    style(attendance): load compact sidebar KPI styles

--- apps/web/src/modules/attendance/AttendanceApp.tsx
+++ apps/web/src/modules/attendance/AttendanceApp.tsx
@@ -1,5 +1,6 @@
 import { useMemo, useState } from 'react';
 import './attendance.css';
+import './attendanceSidebarKpis.css';
 import { getAttendanceInitialConversations, type AttendanceConversation } from './mocks/attendanceMockProvider';
 
 const NAV_ITEMS = [
