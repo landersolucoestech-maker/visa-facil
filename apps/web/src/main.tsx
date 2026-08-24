e0a32b073db3f440a4a39fc0f8f4c738d78a5197
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-24 13:58:05 -0300

    style: load CRM prototype styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,6 +4,7 @@ import { RootApplication } from './RootApplication';
 import './modules/public-site/styles/01-base.css';
 import './modules/public-site/styles/02-sections-responsive.css';
 import './modules/public-site/styles/03-hero-v3.css';
+import './modules/crm/crm.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
